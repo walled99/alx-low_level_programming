d6a0d9b0357cb67750bf869a1c2cfe938d70dd1f
Waleed <walled-sebae@hotmail.com>
2023-06-15 16:31:46 -0700

    print with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - enter point
+ *
+ * Description: program print a statement with puts function
+ *
+ * Return: program return 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
