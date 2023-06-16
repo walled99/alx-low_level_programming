10533475c7ee7fc1a0480c10afd588814a19aaea
Waleed <walled-sebae@hotmail.com>
2023-06-16 15:46:43 -0700

    upd

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
-
 /**
  * main - entry point
  *
- * Description: write lower and upper case
+ * Description: convert string to lower case
  *
- * Return: return 0
- *
+ * Return: return 0 (success)
 */
 
 int main(void)
 {
-	char str[] = "abcdefghijklmnopqrstuvwxyz";
+	char str[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i = 0;
 
-	while (str[i] != '\n')
+	while (str[i] != '\0')
 	{
+		str[i] = str[i] + 32;
 		putchar(str[i]);
 		i++;
 	}
 	i=0;
-	while (str[i] != '\n')
+	while (str[i] != '\0')
 	{
 		str[i] = str[i] - 32;
 		putchar(str[i]);
 		i++;
 	}
+	putchar('\n');
 	return (0);
 }
