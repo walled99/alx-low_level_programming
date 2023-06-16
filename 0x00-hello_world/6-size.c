53923b7656d15645feb3c649e016ee0cfbde901c
Waleed <walled-sebae@hotmail.com>
2023-06-15 17:19:46 -0700

    print types of var

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry points.
+ * 
+ * Description: print types of data types using sizeof function.
+ * 
+ * Return: return 0 (success).
+*/
+
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+	
+	printf("Size of a char: %c byte(s)\n", sizeof(c));
+	printf("Size of an int: %d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
+	printf("Size of a float: %u byte(s)\n", sizeof(f));
+	return (0);
+}
