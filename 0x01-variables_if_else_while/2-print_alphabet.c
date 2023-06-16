a99a3d7d7a1d2d2aa94bf1fc5322473af7ab8310
Waleed <walled-sebae@hotmail.com>
2023-06-16 14:26:51 -0700

    upper to lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - entry point
+ *
+ * Description: convert string to lower case
+ *
+ * Return: return 0 (success)
+*/
+
+int main(void)
+{
+	char str[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i = 0;
+	
+
+	while (str[i] != '\0')
+	{
+		str[i] = str[i] + 32;
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
