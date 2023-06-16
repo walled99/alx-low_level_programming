3e2d774f865852f6066a3ddcd4d8a47c7482e662
Waleed <walled-sebae@hotmail.com>
2023-06-15 17:23:33 -0700

    print types of var

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 	long long int lli;
 	float f;
 	
-	printf("Size of a char: %c byte(s)\n", sizeof(c));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
 	printf("Size of an int: %d byte(s)\n", sizeof(i));
 	printf("Size of a long int: %d byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
