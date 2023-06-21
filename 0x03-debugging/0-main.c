ede1ebebb9d1ee4e80fa75af7ccd413a45ce97f5
Waleed <walled-sebae@hotmail.com>
2023-06-21 16:19:25 -0700

    fix error

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * main - test function that display if num is pos or neg
- *
- * Return: 0 (success)
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
 
 int main(void)
 {
 	int i;
 
-	i = 0;
+	i = 98 - 98;
 	positive_or_negative(i);
-	
+
 	return (0);
 }
