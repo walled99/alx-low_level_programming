8e8c753714dd22d5ab1be10e58aac2310e0bd64c
Waleed <walled-sebae@hotmail.com>
2023-06-21 16:04:39 -0700

    debugging positive,negative fun

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - test function that display if num is pos or neg
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int i = 0;
+	positive_or_negative(i);
+	return (0);
+}
