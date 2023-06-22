f55ef8cc64047c1869e4095e1d1d792ec8c3cec2
Waleed <walled-sebae@hotmail.com>
2023-06-21 19:23:37 -0700

    betty

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -19,7 +19,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month >= 2 && day >= 60) 
+		if (month >= 2 && day >= 60)
 		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
