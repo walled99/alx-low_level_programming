d096cfd29deeae37c9c2d31e5e74889ed864ffb4
Waleed <walled-sebae@hotmail.com>
2023-06-22 08:25:53 -0700

    time two num

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - entry point
+ *
+ * Description: muliplty two integer
+ * @a: first value integer
+ * @b: second integer
+ *
+ * Return: a*b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
