0f5353da65f4836eaf0b7addd3f64c5f7491b4f3
Waleed <walled-sebae@hotmail.com>
2023-06-22 17:29:57 -0700

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_diagonal - entry point
+ *
+ * Description: print diagonal shape
+ * @n: diagonal length
+ *
+ * Return: void
+*/
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			if (i == j)
+			{
+				_putchar(92);
+				_putchar('\n');
+				break;
+			}
+			else
+				_putchar(' ');
+		}
+	}
+	
+}
