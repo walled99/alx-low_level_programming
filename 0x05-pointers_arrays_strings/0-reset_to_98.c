4476ab4ee871e6808b4168739dbbee7f2d60f111
Waleed <walled-sebae@hotmail.com>
2023-06-26 16:04:20 -0700

    0. 98 Battery st. task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - entry point
+ *
+ * Description: function that takes a pointer to
+ * an int as parameter and updatesthe
+ * value it points to to 98
+ * @n: pointer variable hold address
+ *
+ * Return: void
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
