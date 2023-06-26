46ef69b3905d41055e34dd6a4ccaff2edbe0e8ea
Waleed <walled-sebae@hotmail.com>
2023-06-26 16:30:10 -0700

    1. Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int - entry point
+ *
+ * Description: swaps the values of two integers
+ * @a: pointer hold address of var whose type int
+ * @b: pointer hold add of var whose type int
+ *
+ * Return: void
+*/
+
+void swap_int(int *a, int *b)
+{
+	int *tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
