9bc0ea1726d6c40fb387825886af6dab30f1fcf6
Waleed <walled-sebae@hotmail.com>
2023-06-26 19:31:13 -0700

    put2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - start point
+ *
+ * Description: print one and one not
+ * @str: pointer to string
+ *
+ * Return: void
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i); i += 2)
+		_putchar(*(str + i));
+	_putchar('\n');
+}
