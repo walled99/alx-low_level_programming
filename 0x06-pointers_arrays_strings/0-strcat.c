115cc1c6369b58ee4c2f7f426779298a00f41c2d
Waleed <walled-sebae@hotmail.com>
2023-06-29 17:00:25 -0700

    concatenation task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _stract - entry point
+ *
+ * Description: concatenate Strings
+ * @dest: pointer points to String destination
+ * @src: Pointer points to String source
+ *
+ * Return: pointer points to array of char*/
+
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = 0;
+	int src_len = 0;
+	int i;
+	
+	while (dest[dest_len] != '\0')
+	
+		dest_len++;
+	while (src_len != '\0')
+		src_len++;
+	for (i = 0; i <= src_len; i++)
+		dest[dest_len + i] = src[i];
+
+	return (dest);
+}
