bab1ac65493f004c0e4408d39e986fb7206402b3
Waleed <walled-sebae@hotmail.com>
2023-07-05 15:55:02 -0700

    print in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - entry point
+ *
+ * Description: print String in reverse
+ * @s: pointer to string
+ *
+ * Return: void
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_puts_recursion(s + 1);
+	_putchar(*s);
+}
