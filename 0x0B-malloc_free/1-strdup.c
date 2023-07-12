ae5803c16e7528c388ba3b3d87cb4ad71c033e04
Waleed <walled-sebae@hotmail.com>
2023-07-11 18:09:14 -0700

    copy String

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,9 +1,9 @@
 #include "main.h"
-11;rgb:0000/0000/0000#include <stdlib.h>
+#include <stdlib.h>
 
 /**
- * _strdup: Entry point
- *
+ * *_strdup: Entry point
+ * short description: pointer
  * Description: copy String to another.
  * @str: pointer to chars
  *
@@ -18,7 +18,7 @@ char *_strdup(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 		size++;
-	str2 = malloc(sizeof(str) * (size - 1));
+	str2 = malloc(sizeof(char) * size);
 	if (str == NULL || str2 == NULL)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
