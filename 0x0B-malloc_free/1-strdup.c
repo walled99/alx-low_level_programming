06919e3842ce15886225975cbc35c509a45ecc0a
Waleed <walled-sebae@hotmail.com>
2023-07-11 18:17:04 -0700

    copy String

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,10 +16,12 @@ char *_strdup(char *str)
 	int i;
 	char *str2;
 
+	if (str == NULL)
+		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 		size++;
 	str2 = malloc(sizeof(char) * (size + 1));
-	if (str == NULL || str2 == NULL)
+	if (str2 == NULL)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 		*(str2 + i) = str[i];
