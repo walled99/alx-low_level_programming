e598f89e15b3634e030d2a167b992c4457c75eb0
Waleed <walled-sebae@hotmail.com>
2023-07-11 19:33:25 -0700

    concatenate Strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,15 +15,21 @@ char *str_concat(char *s1, char *s2)
 {
 	int i;
 	int j = 0;
-	int size1;
-	int size2 = 1;
+	int size1 = 0;
+	int size2 = 0;
 	char *str3;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		size1++;
 	for (i = 0; s2[i] != '\0'; i++)
 		size2++;
-	str3 = malloc(sizeof(char) * (size1 + size2));
+	str3 = malloc(sizeof(char) * (size1 + size2 + 1));
+	if (str3 == NULL)
+		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++, j++)
 		str3[j] = s1[i];
 	for (i = 0; s2[i] != '\0'; i++, j++)
