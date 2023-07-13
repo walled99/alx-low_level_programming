93150a95e5cc3232d36b02087c4cd03402369d1c
Waleed <walled-sebae@hotmail.com>
2023-07-12 17:42:55 -0700

    multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-11;rgb:0000/0000/0000#include <stdlib.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -14,16 +14,18 @@
 int main(int argc, char *argv[])
 {
 	int result = 0;
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
+	int num1;
+	int num2;
 
-	if (argc == 3)
+	if (argc != 3)
 	{
-		result = num1 * num2;
-		printf("%d\n", result);
-		return (0);
+		printf("Error");
+		printf("\n");
+		return (1);
 	}
-	printf("Error");
-	printf("\n");
-	return (1);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
+	return (0);
 }
