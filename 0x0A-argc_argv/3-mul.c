8fc8fcfb99eda9ad1a9848130b860bcdf40a3df1
Waleed <walled-sebae@hotmail.com>
2023-07-12 17:36:32 -0700

    multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
+11;rgb:0000/0000/0000#include <stdlib.h>
 
 /**
- *main - Entry point
+ * main - Entry point
  *
+ * Description: nultiplication
+ * @argc: argument counter
+ * @argv: argument Vector
  *
- *
- *
- *
+ * Return: 0 , 1
 */
 
 int main(int argc, char *argv[])
 {
 	int result = 0;
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
 
 	if (argc == 3)
 	{
-		result = atoi(argv[1]) * atoi(argv[2]);
+		result = num1 * num2;
 		printf("%d\n", result);
 		return (0);
 	}
