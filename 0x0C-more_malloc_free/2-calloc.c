3432127a8281b548b3039a9a06727e34b841ec00
Waleed <walled-sebae@hotmail.com>
2023-07-13 18:37:55 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,45 +2,25 @@
 #include "main.h"
 
 /**
- * *_memset - Entry point
- * @s: String (array of char)
- * @b: String (array of chars)
- * @n: unsigned integer
- *
- * Return: pointer
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int k;
-
-	for (k = 0; k < n; k++)
-	{
-		s[k] = b;
-	}
-
-	return (s);
-}
-
-/**
- * *_calloc - Entry point
- *
- * @nmemb: Integer
- * @size: Integer
- *
- * Return: pointer
- */
+* _calloc - Entry point
+*
+* @nmemb: Integer
+* @size: Integer
+*
+* Return: pointer
+*/
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ptr;
+	char *p;
+	unsigned int b;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ptr = malloc(size * nmemb);
-
-	else if (ptr == NULL)
+	p = malloc(nmemb * size);
+	if (p == NULL)
 		return (NULL);
-
-	_memset(ptr, 0, nmemb * size);
-	return (ptr);
+	for (b = 0; b < (nmemb * size); b++)
+		p[b] = 0;
+	return (p);
 }
