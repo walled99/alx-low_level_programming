5b0572e5f08b359bff4e0ee2c993a91aa976ee8c
Waleed <walled-sebae@hotmail.com>
2023-07-13 18:15:26 -0700

    allocate memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - Entry point
+ *
+ * @b: unsigned Integer
+ *
+ * Return: pointer 
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
