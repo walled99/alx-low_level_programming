1b1d5e2ee73eba19d7f952c7291f9681995d2bef
Waleed <walled-sebae@hotmail.com>
2023-07-18 17:31:18 -0700

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MY_STRUCT_H
+#define MY_STRUCT_H
+
+/**
+ * struct dog - entry point
+ * @name: pointer char
+ * @age: pointer float
+ * @owner: pointer char
+ * Description: declare dog struct
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* MY_STRUCT_H */
