a687bf338af3d691f05293d4c89d9afb21a7d99a
Waleed <walled-sebae@hotmail.com>
2023-07-18 17:43:56 -0700

    function to create struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - entry point
+ * Description: create struct
+ * @d: pointer
+ * @name: pointer
+ * @age: float
+ * @owner: pointer
+ * Return: void
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
