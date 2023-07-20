d7a3037c9099498419a331eeaa7578593de4552c
Waleed <walled-sebae@hotmail.com>
2023-07-20 15:04:24 -0700

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,6 +13,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ptr;
 
+	va_start(ptr, n);
 	for (i = 0; i < n; i++)
 	{
 		char *str = va_arg(ptr, char*);
