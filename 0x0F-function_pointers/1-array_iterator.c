0ef24cf8962ec0bcc4cdea515f53350be2c7fee3
Waleed <walled-sebae@hotmail.com>
2023-07-19 19:43:51 -0700

    iteratie using pointer function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Entry point
+ * Description: executes a function given as a parameter on each element
+ * @array: pointer
+ * @size: size of array
+ * @action: function pointer
+ * Return: void
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	while (size > 0)
+	{
+		action(*array);
+		array++;
+		size--;
+	}
+}
