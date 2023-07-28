abb240b6b6c8155a38051b3896ed6f5b60fb956e
walled99 <walled-sebae@hotmail.com>
2023-07-28 16:50:24 -0700

    solving Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,10 +1,16 @@
 #include "lists.h"
 
-/***/
+/**
+ * print_list - Entry point
+ * Description: count number of nodes
+ * @h: pointer to struct
+ * Return: unsigned int
+*/
 
 size_t print_list(const list_t *h)
 {
 	unsigned int i = 0;
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
+#ifndef LIST_H
+#define LIST_H
 #include <stdlib.h>
 #include <stdio.h>
-
 
 /**
  * struct list_s - singly linked list
@@ -10,13 +11,17 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 
 size_t print_list(const list_t *h);
 int _putchar(char c);
+
+
+#endif
