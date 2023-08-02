9e80ad87f2f96512f8b6bc12000fd50d5d82b0fc
walled99 <walled-sebae@hotmail.com>
2023-08-01 18:34:13 -0700

    Task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -16,7 +16,7 @@ int pop_listint(listint_t **head)
 	if (*head == NULL || head == NULL)
 		return (0);
 	ptr = *head;
-	num = ptr->next;
+	num = ptr->n;
 	*head = ptr->next;
 	free(ptr);
 	return (num);
