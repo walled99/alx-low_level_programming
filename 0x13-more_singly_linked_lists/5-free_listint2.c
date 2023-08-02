58be60b1172c63bdb651f300e663ad47e376196c
walled99 <walled-sebae@hotmail.com>
2023-08-01 18:04:56 -0700

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Entry point
+ * Description: free list
+ * @head: pointer to pointer list
+ * Return: NULL
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	while (*head != NULL)
+	{
+		ptr = (*head)->next;
+		free(*head);
+		head = ptr;
+	}
+	head = NULL;
+}
