4d21aa79165515ce8efa96c15194e8471b7c1735
walled99 <walled-sebae@hotmail.com>
2023-08-10 17:39:03 -0700

    TASK 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,6 +25,8 @@ int create_file(const char *filename, char *text_content)
 	if (f_d < 0)
 		return (-1);
 	test = write(f_d, text_content, length);
+	if (test < 0)
+		return (-1);
 	close(f_d);
 	return (1);
 }
