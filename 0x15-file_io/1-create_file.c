c97ad13da51c0abfcbf2eb60cf7548e6bb3759aa
walled99 <walled-sebae@hotmail.com>
2023-08-10 17:31:22 -0700

    TASK 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,13 +11,21 @@
 int create_file(const char *filename, char *text_content)
 {
 	int f_d;
-	char buf[1024 * 8];
+	int test;
+	int length = 0;
+	char *text_len;
+
+	text_len = text_content;
+	while (*text_len++)
+		length++;
 
 	if (filename == NULL)
 		return (-1);
 	f_d = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	f_d = write(f_d, buf, sizeof(text_content));
 	if (f_d < 0)
+		return (-1);
+	test = write(f_d, text_content, length);
+	if (test < 0)
 		return (-1);
 	close(f_d);
 	return (1);
